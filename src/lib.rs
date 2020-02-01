eef34eabad5e7f97221a6bf07557ec6432f4cb3b
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-02-01 02:27:26 +0100

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -7,65 +7,61 @@ use wasm_bindgen::prelude::Closure;
 use wasm_bindgen::JsCast;
 
 struct DocumentReady {
-  receiver: oneshot::Receiver<()>,
-  _cb: Closure<FnMut()>,
+    receiver: oneshot::Receiver<()>,
+    _cb: Closure<FnMut()>,
 }
 
 /// A function to call once the DOM has loaded. If the DOM is already loaded,
 /// it will return on next tick.
 impl Future for DocumentReady {
-  type Item = ();
-  type Error = ();
+    type Item = ();
+    type Error = ();
 
-  fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-    match self.receiver.poll() {
-      Ok(Async::Ready(_)) => Ok(Async::Ready(())),
-      Ok(Async::NotReady) => Ok(Async::NotReady),
-      Err(_) => {
-        if cfg!(debug_assertions) {
-          unreachable!("This future keeps the closure alive. The closure keeps the sender alive. Therefor it can't be cancelled.")
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        match self.receiver.poll() {
+            Ok(Async::Ready(_)) => Ok(Async::Ready(())),
+            Ok(Async::NotReady) => Ok(Async::NotReady),
+            Err(_) => {
+                if cfg!(debug_assertions) {
+                    unreachable!("This future keeps the closure alive. The closure keeps the sender alive. Therefor it can't be cancelled.")
+                }
+                Err(())
+            }
         }
-        Err(())
-      }
     }
-  }
 }
 
 /// Wait for the DOM to be loaded.
 pub fn ready() -> impl Future<Item = (), Error = ()> {
-  let doc = window()
-    .document()
-    .expect("should have a document on window");
+    let doc = window()
+        .document()
+        .expect("should have a document on window");
 
-  let (sender, receiver) = oneshot::channel();
+    let (sender, receiver) = oneshot::channel();
 
-  let mut sender = Some(sender);
-  let cb = move || {
-    sender.take().unwrap().send(()).unwrap();
-  };
+    let mut sender = Some(sender);
+    let cb = move || {
+        sender.take().unwrap().send(()).unwrap();
+    };
 
-  let cb = Closure::wrap(Box::new(cb) as Box<FnMut()>);
-  match doc.ready_state().as_str() {
-    "complete" | "interactive" => {
-      window()
-        .set_timeout_with_callback(cb.as_ref().unchecked_ref())
-        .unwrap();
-    }
-    _ => {
-      doc
-        .add_event_listener_with_callback(
-          "DOMContentLoaded",
-          cb.as_ref().unchecked_ref(),
-        )
-        .unwrap();
-    }
-  };
+    let cb = Closure::wrap(Box::new(cb) as Box<FnMut()>);
+    match doc.ready_state().as_str() {
+        "complete" | "interactive" => {
+            window()
+                .set_timeout_with_callback(cb.as_ref().unchecked_ref())
+                .unwrap();
+        }
+        _ => {
+            doc.add_event_listener_with_callback("DOMContentLoaded", cb.as_ref().unchecked_ref())
+                .unwrap();
+        }
+    };
 
-  DocumentReady { _cb: cb, receiver }
+    DocumentReady { _cb: cb, receiver }
 }
 
 /// Access the window.
 // todo: expect_throw
 fn window() -> web_sys::Window {
-  web_sys::window().expect("no global `window` exists")
+    web_sys::window().expect("no global `window` exists")
 }
